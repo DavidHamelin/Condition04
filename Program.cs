44d60ef7a9046b35d7a7aba5911c168b8c0a55c3
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 16:10:29 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -10,16 +10,24 @@ namespace Condition04
     {
         static void Main(string[] args)
         {
+            // Les identifiants sont:
             string login = "LaManu";
             string password = "bienvenue";
-
+            // On demande à l'utilisateur de saisir son identifiant et mdp
             Console.WriteLine("Entrez votre identifiant : ");
             string userLogin = (Console.ReadLine());
             Console.WriteLine("Entrez votre mot de passe : ");
             string userPassword = (Console.ReadLine());
-
-            if (userLogin == login && userPassword == password) Console.WriteLine("Bienvenue à La Manu Le Havre !");
-            else Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+            // Si l'id et le mdp sont exacts alors on affiche un message de bienvenue
+            if (userLogin == login && userPassword == password)
+            {
+                Console.WriteLine("Bienvenue à La Manu Le Havre !");
+            }
+            // Sinon on indique qu'il y a une erreur quelque part
+            else
+            {
+                Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+            }
         }
     }
 }
