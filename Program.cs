ff9ff3f419fac7e5b0b7b4b31c3fc020829ba31f
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 11:04:56 +0100

    ex 04 condition

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "LaManu";
+            string password = "bienvenue";
+
+            Console.WriteLine("Entrez votre identifiant : ");
+            string userLogin = (Console.ReadLine());
+            Console.WriteLine("Entrez votre mot de passe : ");
+            string userPassword = (Console.ReadLine());
+
+            if (userLogin == login && userPassword == password) Console.WriteLine("Bienvenue à La Manu Le Havre !");
+            else Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+        }
+    }
+}
